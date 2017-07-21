2184d498949696d11c0ef389d014b5af690f4c71
Inga Lovinde <52715130+inga-lovinde@users.noreply.github.com>
2017-07-21 12:37:50 +0300

    Added empty TargetMetadataStorage tests

---
+++ Tests/TargetMetadataStorage/InMemoryMetadataStorage/StorageTests.cs
@@ -0,0 +1,21 @@
+﻿namespace EternalArrowBackup.TargetBinaryStorage.InMemoryBinaryStorage.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
+    using Xunit;
+
+    public static class StorageTests
+    {
+        [Fact]
+        [Trait("Category", "Simple")]
+        public static void CheckStorage()
+        {
+            Assert.True(true);
+        }
+    }
+}
